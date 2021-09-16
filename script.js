9ba73ff6fe9c0e5c50ad4dfa169ad5831eb0b99a
Arsia <cam.lucas89@gmail.com>
2021-09-16 19:11:29 +0200

    add JS 1 and 2 quest

---
+++ script.js
@@ -0,0 +1,27 @@
+/**
+* Hello toi ! Bienvenue sur ce simple exercice pour tes débuts en JS
+* Pour le moment, il n'y a rien d'intéressant.
+* Je t'invite à revenir un peu plus tard.
+* Salut */
+console.log("Hello world !");
+console.log("Hi, world");
+
+var myName = "Camille";
+console.log(myName);
+
+let myBudget = 0;
+myBudget ++;
+console.log(myBudget);
+
+myBudget += 2;
+console.log(myBudget);
+
+myBudget --;
+console.log(myBudget);
+
+myBudget -= 2;
+console.log(myBudget);
+
+let hello = "Hello";
+hello += ", World !";
+console.log(hello);
